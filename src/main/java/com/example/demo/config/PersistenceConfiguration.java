82eb040f27b3baf186ad2ae6d4164c1b27cbb2d4
George <george.v@gmail.com>
2021-07-02 11:46:36 +0300

    Commented out configuration bean for datasource

--- src/main/java/com/example/demo/config/PersistenceConfiguration.java
+++ src/main/java/com/example/demo/config/PersistenceConfiguration.java
@@ -9,11 +9,11 @@ import javax.sql.DataSource;
 @Configuration
 public class PersistenceConfiguration {
 
-    @Bean
-    public DataSource dataSource() {
-        DataSourceBuilder builder = DataSourceBuilder.create();
-        builder.url("jdbc:postgresql://localhost:5432/conference_app?user=postgres&password=password");
-        return builder.build();
-    }
+//    @Bean
+//    public DataSource dataSource() {
+//        DataSourceBuilder builder = DataSourceBuilder.create();
+//        builder.url("jdbc:postgresql://localhost:5432/conference_app?user=postgres&password=password");
+//        return builder.build();
+//    }
 
 }
