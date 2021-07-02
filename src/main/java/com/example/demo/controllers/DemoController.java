8896a74c66f5bd43f9b17fc0b525d9417edaadf8
George <george.v@gmail.com>
2021-07-02 11:28:55 +0300

    initial commit

---
+++ src/main/java/com/example/demo/controllers/DemoController.java
@@ -0,0 +1,27 @@
+package com.example.demo.controllers;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Slf4j
+@Controller
+public class DemoController {
+
+    @ResponseBody
+    @GetMapping("demo")
+    public String demo(){
+        log.info("Hello Spring");
+        return "Hello Spring boot";
+    }
+
+    @GetMapping("welcome")
+    public String welcome(Model model){
+        log.info("welcome method called!");
+        model.addAttribute("message", "Welcome to Spring boot");
+        return "welcome";
+    }
+
+}
---
+++ src/main/java/com/example/demo/models/Speaker.java
@@ -0,0 +1,36 @@
+package com.example.demo.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Type;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@Entity(name = "speakers")
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+public class Speaker {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long speaker_id;
+
+    private String first_name;
+    private String last_name;
+    private String title;
+    private String company;
+    private String speaker_bio;
+
+    @Lob
+    @Type(type = "org.hibernate.type.BinaryType")
+    private byte[] speaker_photo;
+
+    @ManyToMany(mappedBy = "speakers")
+    @JsonIgnore
+    private List<Session> sessions;
+}
+
