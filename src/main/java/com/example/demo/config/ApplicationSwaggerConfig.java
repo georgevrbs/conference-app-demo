29f6d7b8a8a3c0a43c65ce2e6ffd2e8a9a5ae04a
George <george.v@gmail.com>
2021-07-05 12:38:58 +0300

    Added springfox to project

---
+++ src/main/java/com/example/demo/config/ApplicationSwaggerConfig.java
@@ -0,0 +1,34 @@
+package com.example.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+public class ApplicationSwaggerConfig {
+
+    @Bean
+    public Docket speakerApi(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(getApiInfo());
+    }
+
+    private ApiInfo getApiInfo() {
+        return new ApiInfoBuilder()
+                .title("Conference API")
+                .version("2.0")
+                .description("API for managing Conference Sessions and Speakers")
+                .contact(new Contact("George Testing", "http://confsessions.com", "georgeConf@mail.com"))
+                .build();
+    }
+}
